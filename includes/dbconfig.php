723b19b95acce670e208ffdeea0e2c3c6012b16d
Hannes Sachsenmaier <1910653347@stud.fh-kufstein.ac.at>
2020-04-01 19:45:12 +0200

    changed database to snakegame, changed dbconfig

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'myproject');
-define('DB_USER', 'snakeUser');
+define('DB_NAME', 'snakeGame');
+define('DB_USER', 'snakeDbUser');
 define('DB_PASS', 'snake12345!');
